11a7de5eaba8fb25e4073b0df5a1b3cdfd94133d
Billur <haskarab@msu.edu>
2025-02-28 18:57:54 -0500

    First push B)

---
+++ app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Home.java
@@ -0,0 +1,31 @@
+package edu.msu.cse476.baragurr.ecosnap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onStartCamera(View view) {
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
+    }
+}
