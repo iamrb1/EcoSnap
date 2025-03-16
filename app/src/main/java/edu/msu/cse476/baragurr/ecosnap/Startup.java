731ff4f591b4c6724646c81ff7401df7e6ef68b5
Billur <haskarab@msu.edu>
2025-03-16 18:36:14 -0400

    Added Startup - KayLyn Nuttall's Code

--- app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Startup.java
+++ app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Startup.java
@@ -1,24 +1,45 @@
 package edu.msu.cse476.baragurr.ecosnap;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.msu.cse476.baragurr.ecosnap.R;
 
 public class Startup extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_startup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(Startup.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Startup.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
+    public void onStartHome(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
