a8194c3843aee004a336127dc8b82654456df062
Alex Chirillo <144390653+achirillo@users.noreply.github.com>
2025-03-17 21:27:24 -0400

    Add profile page

--- app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Account.java
+++ app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Account.java
@@ -21,4 +21,6 @@ public class Account extends AppCompatActivity {
             return insets;
         });
     }
+
+
 }
--- app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Profile.java
+++ app/src/main/java/edu/msu/cse476/baragurr/ecosnap/Profile.java
@@ -1,6 +1,9 @@
 package edu.msu.cse476.baragurr.ecosnap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
+
+    String username = "User";
+
+    int num_recycled = 200;
+    TextView user_text;
+    TextView recycle_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,33 @@ public class Profile extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        user_text = findViewById(R.id.username_welcome_text);
+
+        String username_formatted = getString(R.string.username_welcome, username);
+        user_text.setText(username_formatted);
+
+        recycle_text = findViewById(R.id.recycle_count_text);
+
+        String recycle_formatted = getString(R.string.recycle_count, num_recycled);
+        recycle_text.setText(recycle_formatted);
+
+    }
+
+    public void onStartCamera(View view) {
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
+    }
+    public void onStartLeaderboard(View view) {
+        Intent intent = new Intent(this, Leaderboard.class);
+        startActivity(intent);
+    }
+    public void onStartProfile(View view) {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+    public void onStartHome(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
